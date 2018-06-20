ecb9898245a53839c58e04d43a4a04deb031af99
DevExpressExampleBot <fakeEmail>
2018-06-20 18:57:31 +0300

    Source auto update [skip ci]

---
+++ VB/DXWebApplication1/DXWebApplication1/App_Start/FilterConfig.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Web
+
+Namespace DXWebApplication1
+    Public Class FilterConfig
+        Public Shared Sub RegisterGlobalFilters(ByVal filters As GlobalFilterCollection)
+            filters.Add(New HandleErrorAttribute())
+        End Sub
+    End Class
+End Namespace
---
+++ VB/DXWebApplication1/DXWebApplication1/Controllers/HomeController.vb
@@ -0,0 +1,24 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+
+Namespace DXWebApplication1.Controllers
+    Public Class HomeController
+        Inherits Controller
+
+        Public Function Index() As ActionResult
+            ViewBag.Message = "Welcome to DevExpress Extensions for ASP.NET MVC!"
+
+            Return View()
+        End Function
+
+        Private db As New DXWebApplication1.Models.Nwind_SalesDataEntities()
+
+        <ValidateInput(False)> _
+        Public Function PivotGridPartial() As ActionResult
+            Dim model = db.SalesPersons
+            Return PartialView("_PivotGridPartial", model.ToList())
+        End Function
+    End Class
+End Namespace
